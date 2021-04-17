b78fa4fb261140c726ba709f2811c7b17295f029
yishayweb <yishayweb@gmail.com>
2021-04-17 16:49:10 +0300

    added console to marketing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
 export default () => {
+  console.log("hello");
   return (
     <div>
       <StylesProvider>
